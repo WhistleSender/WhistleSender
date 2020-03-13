860b5fb6962b384460a4a76f00de27d9b920d5fa
WhistleSender <whistlesender@gmail.com>
2020-03-14 01:31:57 +0800

    build

--- utils/convert_and_compress_assets.bash
+++ utils/convert_and_compress_assets.bash
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 gm mogrify -compress JPEG2000 -quality 80 -format jpg ../archive/*.jpeg
 gm mogrify -compress JPEG2000 -quality 80 -format jpg ../archive/*.webp
 gm mogrify -compress JPEG2000 -quality 80 -format jpg ../archive/*.png
 rm ../archive/*.jpeg
 rm ../archive/*.webp
 rm ../archive/*.png
-read -p "Press Any Key To Exit"
+read -p "Press enter to continue"
