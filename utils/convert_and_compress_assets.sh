03ca009f65f65cb80da1a80a211b7ab3a970a239
ncovmemory <2019ncovmemory@gmail.com>
2020-03-21 19:14:37 +0800

    build

--- utils/convert_and_compress_assets.sh
+++ utils/convert_and_compress_assets.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# for i in ../archive/*.mp4;
+# do name=`echo "$i" | cut -d'.' -f1`
+#     echo "$name"
+#     ffmpeg -i "$i" -vcodec h264 -acodec mp3 "$i"
+# done
 gm mogrify -compress JPEG2000 -quality 80 -format jpg ../archive/*.jpeg
 gm mogrify -compress JPEG2000 -quality 80 -format jpg ../archive/*.webp
 gm mogrify -compress JPEG2000 -quality 80 -format jpg ../archive/*.png
